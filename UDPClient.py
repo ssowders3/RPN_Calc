f49d6e18764aa3e79e7f84f29c57e0559aa3728b
ssowders3 <ssowders96@gmail.com>
2018-02-13 17:28:41 -0500

    Working UDP Client and Server code with retry

--- UDPClient.py
+++ UDPClient.py
@@ -4,6 +4,7 @@ import sys  # for exit
 # create dgram udp socket
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(2)
 except socket.error:
     print 'Failed to create socket'
     sys.exit()
@@ -15,6 +16,7 @@ try:
 except:
     raise Exception("Error occurred while reading parameters")
 
+try_count = 0
 while 1:
     tokens, stack = expression.split(' '), []
     for tk in tokens:
@@ -25,14 +27,23 @@ while 1:
             except:
                 raise Exception("Unable to pop operands off Stack due to invalid Expression input")
             message = str(operand1 + " " + operand2 + " " + tk)
-            try:
-                s.sendto(message, (ip_address, port_number))
-                d = s.recvfrom(1024)
-                data = d[0]
-                addr = d[1]
-            except socket.error, msg:
-                print 'Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-                sys.exit()
+            while 1:
+                try:
+                    print try_count
+                    if (try_count < 3):
+                        s.sendto(message, (ip_address, port_number))
+                        try_count = try_count + 1
+                        s.settimeout(2)
+                        d = s.recvfrom(4094)
+                        data = d[0]
+                        addr = d[1]
+                        break
+
+                except socket.timeout:
+                    print 'send failed, trying again'
+                    if try_count == 3:
+                        print 'failed to send closing socket'
+                        sys.exit()
             stack.append(float(data))
         else:
             stack.append(float(tk))
